c5dc177cff97477099d0f7ea08347cebd6a114e9
Alexander Lamp <alexander.mlamp@gmail.com>
2020-03-30 00:41:23 -0700

    updating files

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,26 @@
-// import the data from data.js
+// from data.js
 const tableData = data;
 
-// Reference the HTML table using d3
+// get table references
 var tbody = d3.select("tbody");
 
 function buildTable(data) {
-    tbody.html("");
+  // First, clear out any existing data
+  tbody.html("");
 
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
   data.forEach((dataRow) => {
+    // Append a row to the table body
     let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
     Object.values(dataRow).forEach((val) => {
-        let cell = row.append("td");
-        cell.text(val);  
-    }
-  );
- });
+      let cell = row.append("td");
+      cell.text(val);
+    });
+  });
 }
 
 // Keep track of all filters
@@ -56,8 +62,22 @@ function updateFilters() {
     } else {
       filters['shape'] = null
     };
+  // let filteredData = tableData;
+  // Check to see if a date was entered and filter the
+  // data using that date.
+  
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    
+  // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  // Save the element, value, and id of the filter that was changed
+  // If a filter value was entered then add that filterId and value
+  // to the filters list. Otherwise, clear that filter from the filters object
 
-    filterTable();
+  // Call function to apply all filters and rebuild the table
+  filterTable();
 }
 
 function filterTable() {
@@ -82,8 +102,11 @@ function filterTable() {
    
   });
 
-  buildTable(filteredData);
-}
+  // Loop through all of the filters and keep any data that
+  // matches the filter values
+  // Finally, rebuild the table using the filtered Data
+    buildTable(filteredData);
+  }
 
 // Attach an event to listen for changes to each filter
 // Hint: You'll need to select the event and what it is listening for within each set of parenthesis
